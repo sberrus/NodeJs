8dc0500881445c78d60f74cbec66261c20ab90e5
Samuel Berrus <samuelberrus@gmail.com>
2021-09-11 01:50:39 +0200

    Finalizada Sección 4, Inicio Sección 5, Nuevo formato de orden de clases

--- 04-Bases_de_node/app.js
+++ 04-Bases_de_node/app.js
@@ -1,13 +1,13 @@
 //Dependencias
+const colors = require("colors");
 const { imprimirTabla } = require("./helpers/multiplicar");
-const argv = require("yargs").argv;
+const argv = require("./config/yargs");
+
 //Limpiando consola
 console.clear();
 
 //TODO: Crear un comando que nos permite condicionar si se muestra o no la tabla en la linea de comandos.
 
-/* 
-imprimirTabla(base)
-    .then((namefile) => console.log(namefile, "creado \n"))
+imprimirTabla(argv.b, argv.l, argv.p)
+    .then((namefile) => console.log(namefile, "creado \n".brightGreen.bold))
     .catch((err) => console.log(err));
- */
--- 04-Bases_de_node/helpers/multiplicar.js
+++ 04-Bases_de_node/helpers/multiplicar.js
@@ -1,23 +1,30 @@
+const colors = require("colors");
 const fs = require("fs");
 
-imprimirTabla = async (base = 1, max = 10) => {
+imprimirTabla = async (base = 1, max = 10, print = false) => {
     //File Config
     try {
         const nameFile = `tabla-${base}.txt`;
         const path = "tablas/";
         let salida = "";
+        let salidaPrint = "";
 
         salida += `=============\nTabla del ${base}\n=============\n`;
+        salidaPrint += `=============\nTabla del ${base}\n=============\n`.brightBlue;
         for (let i = 1; i <= max; i++) {
             const res = i * base;
-            salida += `${base} x ${i} = ${res}\n`;
+            salidaPrint += `${base}` + " x ".cyan + `${i} = ${res}\n`;
+            salida += `${base}` + " x " + `${i} = ${res}\n`;
         }
         try {
             fs.writeFileSync(path + nameFile, salida);
         } catch (err) {
             throw err;
         }
-        return salida + "\n" + nameFile;
+        if (print) {
+            return salidaPrint + "\n" + nameFile.brightGreen.italic;
+        }
+        return nameFile.brightGreen.italic;
     } catch (err) {
         throw err;
     }
