caefb409f64a8d44b148cab38b04db17b55cfbb1
Samuel Berrus <samuelberrus@gmail.com>
2021-09-13 11:48:51 +0200

    Sección 5 - 53

--- 05-Aplicacion-Interactiva/helpers/inquirer.js
+++ 05-Aplicacion-Interactiva/helpers/inquirer.js
@@ -33,6 +33,7 @@ const inquirerMenu = async () => {
 
     console.log(consoleHeader("Seleccione una opción:"));
 
+    //Destructuramos y obtenemos lo que nos devuelve la propiedad {opcion} que es la que contendra el valor que nos devuelve el usuario como input. Hay que destacar que la propiedad va a tener el mismo namespace que la propiedad {name:"algo"} del objeto que contiene la configuración del inquirer.
     const { opcion } = await inquirer.prompt(preguntas);
     return opcion;
 };
@@ -47,4 +48,23 @@ const pausa = async () => {
     ]);
 };
 
-module.exports = { inquirerMenu, pausa };
+const leerInput = async (message) => {
+    question = [
+        {
+            type: "input",
+            name: "desc",
+            message,
+            //validate es un callback que nos permite validar los campos de la pregunta. Recibe como argumento el valor que devuelve el usuario en este caso "value".
+            validate(value) {
+                if (value.length === 0) {
+                    return "Por favor ingrese un error".red.bold;
+                }
+                return true;
+            },
+        },
+    ];
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+};
+
+module.exports = { inquirerMenu, pausa, leerInput };
--- 05-Aplicacion-Interactiva/models/tareas.js
+++ 05-Aplicacion-Interactiva/models/tareas.js
@@ -1,7 +1,14 @@
+const Tarea = require("./tarea");
+
 class Tareas {
     _listado = {};
     constructor() {
         this._listado = {};
+    }
+
+    crearTarea(desc = "") {
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
     }
 }
 
