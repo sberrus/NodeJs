690b92bf1136250cdd4142d14fc5084eca7c52e2
Samuel Berrus <samuelberrus@gmail.com>
2021-09-21 19:20:12 +0200

    S6 - C73: Creando instancia de axios para consulta y configurando variables de entorno

--- 06-Aplicacion-clima/models/busquedas.js
+++ 06-Aplicacion-clima/models/busquedas.js
@@ -1,15 +1,28 @@
 const axios = require("axios");
+require("dotenv").config();
 
 class Busquedas {
     historial = [];
     constructor() {
         //TODO: leer DB si existe
     }
+
+    get paramsMapbox() {
+        return {
+            access_token: process.env.MAPBOX_KEY,
+            limit: 5,
+            lenguage: "es",
+        };
+    }
+
     async ciudad(lugar = "") {
         try {
-            const resp = await axios.get(
-                "https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?access_token=pk.eyJ1Ijoic2JlcnJ1cyIsImEiOiJja3R1Ymc4eDQwZ3c3Mm9xdGtiMzVsajNkIn0.whfvjveZK2N0MVYeEBMWFw"
-            );
+            //Petición http
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
+            });
+            const resp = await instance.get();
             console.log(resp.data);
         } catch (error) {
             console.log(error);
