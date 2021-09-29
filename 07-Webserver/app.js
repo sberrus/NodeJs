c45b79c015152b66d2b02ffbec2e2d3b836e14e4
Samuel Berrus <samuelberrus@gmail.com>
2021-09-29 18:42:38 +0200

    S7 - C93: Usando parciales de hbs para ordenar el código

--- 07-Webserver/app.js
+++ 07-Webserver/app.js
@@ -8,7 +8,16 @@ app.set("view engine", "hbs");
 app.use(express.static("public")); //Definimos la ruta de los archivos publicos que ofrecera el servidor
 
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render(
+        //desc: Como primer argumento, partiendo de la ruta predeterminada "./views" con el método render buscamos el archivo que deseamos renderizar. No hace falta usar la extensión para el archivo.
+        "home",
+        //desc: Como segundo argumento enviamos un objeto que contendra información que deseamos enviar al documento que se va a renderizar. Hay que verlo como una prop de react que se envia al template para renderizar información.
+        {
+            nombre: "Samuel Berrus",
+            title: "Curso de Node",
+            h1: "Texto enviado desde el server",
+        }
+    );
 });
 
 app.get("/generic", (req, res) => {
