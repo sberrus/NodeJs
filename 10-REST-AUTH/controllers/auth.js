c98636bb4e5f09436a81500f6bc24c26679b23f6
samdev <samuelberrus@gmail.com>
2021-10-27 19:36:53 +0200

    S10 - C140: Configuración inicial de rutas de login

---
+++ 10-REST-AUTH/controllers/auth.js
@@ -0,0 +1,5 @@
+const login = (req = request, res = response) => {
+	res.send("hola routerrrr");
+};
+
+module.exports = { login };
--- 10-REST-AUTH/models/server.js
+++ 10-REST-AUTH/models/server.js
@@ -3,43 +3,46 @@ const cors = require("cors");
 const { dbConection } = require("../database/config");
 
 class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.usersPath = "/api/users";
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+		this.usersPath = "/api/users";
+		this.authPath = "/api/auth";
 
-        //DB Conection
-        this.conectarDB();
+		//DB Conection
+		this.conectarDB();
 
-        //Middlewares
-        this.middlewares();
+		//Middlewares
+		this.middlewares();
 
-        //Rutas de la app
-        this.routes();
-    }
+		//Rutas de la app
+		this.routes();
+	}
 
-    async conectarDB() {
-        await dbConection();
-    }
+	async conectarDB() {
+		await dbConection();
+	}
 
-    middlewares() {
-        //Directorio público
-        this.app.use(express.static("public"));
+	middlewares() {
+		//Directorio público
+		this.app.use(express.static("public"));
 
-        //Cors config
-        this.app.use(cors());
+		//Cors config
+		this.app.use(cors());
 
-        //Lectura y parseo del body
-        this.app.use(express.json());
-    }
+		//Lectura y parseo del body
+		this.app.use(express.json());
+	}
 
-    routes() {
-        this.app.use(this.usersPath, require("../routes/user"));
-    }
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en puerto ${this.port}`);
-        });
-    }
+	routes() {
+		//Se suele ordenar las rutas por orden alfabético.
+		this.app.use(this.authPath, require("../routes/auth"));
+		this.app.use(this.usersPath, require("../routes/user"));
+	}
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log(`Servidor corriendo en puerto ${this.port}`);
+		});
+	}
 }
 module.exports = Server;
