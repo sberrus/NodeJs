e903f8115b1a7e694084726d62a4a9b27e536fe1
samdev <samuelberrus@gmail.com>
2021-10-28 15:45:01 +0200

    S10 - C141: Creando validaciones para verificar que el inicio de sesión sea correcto antes de generar el JWT

--- 10-REST-AUTH/controllers/auth.js
+++ 10-REST-AUTH/controllers/auth.js
@@ -1,5 +1,46 @@
-const login = (req = request, res = response) => {
-	res.send("hola routerrrr");
+//Imports
+const { response } = require("express");
+const bcryptjs = require("bcryptjs");
+//Schemas
+const Usuario = require("../models/usuario");
+
+const login = async (req = request, res = response) => {
+	const { correo, password } = req.body;
+
+	try {
+		//VALIDACIONES:
+		//existe correo
+		const usuario = await Usuario.findOne({ correo });
+		if (!usuario) {
+			return res
+				.status(400)
+				.json({ msg: "Error al iniciar sesión - Correo" });
+		}
+		//usuario activo
+		if (usuario.estado === false) {
+			return res
+				.status(400)
+				.json({ msg: "Error al iniciar sesión - Estado" });
+		}
+		//contraseña
+		const validPassword = bcryptjs.compareSync(password, usuario.password);
+		if (!validPassword) {
+			return res
+				.status(400)
+				.json({ msg: "Error al iniciar sesión - Password" });
+		}
+		//generar JWT
+
+		res.json({
+			correo,
+			password,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: "Error en el servidor",
+		});
+	}
 };
 
 module.exports = { login };
