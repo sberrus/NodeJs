0bc80a0bb0c69dfeaed67d063aeaa3d01d9d2771
samdev <samuelberrus@gmail.com>
2021-11-26 13:14:38 +0100

    159: Configurado boton de signout de google

--- 11-Google-Sign-In FrontBack/controllers/auth.js
+++ 11-Google-Sign-In FrontBack/controllers/auth.js
@@ -51,13 +51,46 @@ const googleSignIn = async (req, res = response) => {
 	const { id_token } = req.body;
 
 	try {
+		//enviamos el token que nos devuelve el cliente al haberse loggeado con el boton de google y extraemos la información de dicho token con los métodos de autenticación en el backend de google.
 		const { nombre, img, correo } = await googleVerify(id_token);
 
+		//Comprobando si el correo ya esta registrado en la bbdd.
+		let usuario = await Usuario.findOne({ correo });
+
+		if (!usuario) {
+			//Crear usuario
+			const data = {
+				//datos de google
+				nombre,
+				correo,
+				//enviamos un valor por defecto. El usuario nunca va a poder hacer login con una contraseña debido a que al pasar esto por bcrypt nunca hará match.
+				password: ":P",
+				img,
+				//propiedad para lógica de autenticación
+				google: true,
+				role: "USER_ROLE"
+			};
+			usuario = new Usuario(data);
+			await usuario.save();
+		}
+
+		//Si el usuario en DB tiene el estado en false
+		if (!usuario.estado) {
+			return res.status(401).json({
+				ok: false,
+				msg: "Hable con el administrador, usuario bloqueado",
+			});
+		}
+
+		//Generar el jwt
+		const token = await generarJWT(usuario.id);
+
 		res.json({
-			msg: "Todo Okey!",
-			id_token,
+			usuario,
+			token,
 		});
 	} catch (error) {
+		console.log(error);
 		res.status(400).json({
 			ok: false,
 			msg: "Token no se ha verificado correctamente",
